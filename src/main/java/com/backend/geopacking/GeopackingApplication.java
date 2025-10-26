5e7c81fe87e934ea0e290eef65b14c66b3cc7413
Espinozaje <jeisson12aaron@gmail.com>
2025-10-26 14:48:30 -0500

    first commit

---
+++ src/main/java/com/backend/geopacking/GeopackingApplication.java
@@ -0,0 +1,13 @@
+package com.backend.geopacking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeopackingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeopackingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/backend/geopacking/GeopackingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.geopacking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeopackingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
